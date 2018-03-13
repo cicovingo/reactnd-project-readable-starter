104136d8b89a3e1a0181d11f1bae790c6da0c14c
cicovingo <sukruokul@gmail.com>
2018-03-13 16:15:13 +0300

    Update Category.js

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 import PostList from './PostList';
 import AppToolbar from './AppToolbar';
 const Category = (props) => {
-  const { changeOrder, comments, decScoreForPost, deletingPost, filter, incScoreForPost, openEditPostModal, posts, sort } = props;
+  const { changeOrder2, comments2, decScoreForPost2, deletingPost2, filter2, incScoreForPost2, openEditPostModal2, posts2, sort2 } = props;
   return (
     <div>    
-      <AppToolbar sortingTitle="Order" changeOrder={changeOrder} />
-      <PostList comments={comments} decScoreForPost={decScoreForPost} deletingPost={deletingPost} filter={filter} incScoreForPost={incScoreForPost} openEditPostModal={openEditPostModal} posts={posts} sort={sort} />
+      <AppToolbar sortingTitle5="Order" changeOrder5={changeOrder2} />
+      <PostList comments4={comments2} decScoreForPost4={decScoreForPost2} deletingPost4={deletingPost2} filter4={filter2} incScoreForPost4={incScoreForPost2} openEditPostModal4={openEditPostModal2} posts4={posts2} sort4={sort2} />
     </div>
   );
 };
 
-Category.propTypes = { changeOrder: PropTypes.func.isRequired, comments: PropTypes.object.isRequired, decScoreForPost: PropTypes.func.isRequired, deletingPost: PropTypes.func.isRequired, filter: PropTypes.string.isRequired, incScoreForPost: PropTypes.func.isRequired, openEditPostModal: PropTypes.func.isRequired, posts: PropTypes.array.isRequired, sort: PropTypes.object.isRequired }
+Category.propTypes = { changeOrder2: PropTypes.func.isRequired, comments2: PropTypes.object.isRequired, decScoreForPost2: PropTypes.func.isRequired, deletingPost2: PropTypes.func.isRequired, filter2: PropTypes.string.isRequired, incScoreForPost2: PropTypes.func.isRequired, openEditPostModal2: PropTypes.func.isRequired, posts2: PropTypes.array.isRequired, sort2: PropTypes.object.isRequired }
 export default Category;
