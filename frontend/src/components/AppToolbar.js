05fe738d1b45ecd356d9e55ddccee61977e5ae2e
cicovingo <sukruokul@gmail.com>
2018-03-13 16:16:01 +0300

    Update AppToolbar.js

--- frontend/src/components/AppToolbar.js
+++ frontend/src/components/AppToolbar.js
@@ -1,63 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Toolbar, ToolbarGroup, ToolbarTitle } from 'material-ui/Toolbar';
-import MenuItem from 'material-ui/MenuItem';
+import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
 import DropDownMenu from 'material-ui/DropDownMenu';
-const AppToolbar = ({ categories, filter, filterFunc, filterTitle, sort, changeOrderFunc, sortingTitle }) => {
-  this.filterFunction = filterFunc;
-  this.sort = sort;
-  this.changeOrderFunc = changeOrderFunc;
-  const onCategoryFilterChanged = (event, index, value) => {
-    this.filterFunction(value);
-  }
-  const onSortOrderChanged = (event, index, value) => {
-    const newSort = {
-      field: this.sort.field,
-      order: value
-    };
-    this.changeOrderFunc(newSort);
-  }
+const AppToolbar = ({ filterFunc5, sort5, changeOrderFunc5 }) => {
+  this.filterFunc5 = filterFunc5;
+  this.sort5 = sort5;
+  this.changeOrderFunc5 = changeOrderFunc5;
   const onSortFieldChanged = (event, index, value) => {
     const newSort = {
       field: value,
       order: this.sort.order
     };
-    this.changeOrderFunc(newSort);
+    this.changeOrderFunc5(newSort);
   }
   return (
-    <Toolbar>    
-      { categories !== undefined &&
+    <Toolbar>
       <ToolbarGroup >
-        <ToolbarTitle text={filterTitle} />
-        <DropDownMenu value={filter} onChange={onCategoryFilterChanged}>
-          <MenuItem value="" primaryText="All" key="All" />
-          { categories.map((category) =>
-          <MenuItem value={category.name} primaryText={category.name} key={category.name} />
-          )}
+        <DropDownMenu value={"voteScore"} onChange={onSortFieldChanged}>
         </DropDownMenu>
-      </ToolbarGroup>
-      }
-      <ToolbarGroup >
-        <ToolbarTitle text={sortingTitle} />
-        <DropDownMenu value={sort.field} onChange={onSortFieldChanged}>
-          <MenuItem value="voteScore" primaryText="Votes" />
-          <MenuItem value="timestamp" primaryText="Timestamp" />
-        </DropDownMenu>
-        <DropDownMenu value={sort.order} onChange={onSortOrderChanged}>
-          <MenuItem value={'asc'} primaryText="Ascending" />
-          <MenuItem value={'desc'} primaryText="Descending" />
+    	<DropDownMenu value={"timestamp"} onChange={onSortFieldChanged}>
         </DropDownMenu>
       </ToolbarGroup>      
     </Toolbar>
   );
 }
-AppToolbar.propTypes = {
-  categories: PropTypes.array,
-  filter: PropTypes.string,
-  filterFunc: PropTypes.func,
-  filterTitle: PropTypes.string,
-  sort: PropTypes.object.isRequired,
-  changeOrderFunc: PropTypes.func.isRequired,
-  sortingTitle: PropTypes.string.isRequired
-}
+AppToolbar.propTypes = { filterFunc5: PropTypes.func, sort5: PropTypes.object.isRequired, changeOrderFunc5: PropTypes.func.isRequired }
 export default AppToolbar;
