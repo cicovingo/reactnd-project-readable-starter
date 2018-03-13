1501a10fdda99c1049b4dbda125fc93045ac629d
cicovingo <sukruokul@gmail.com>
2018-03-13 16:15:40 +0300

    Update Root.js

--- frontend/src/components/Root.js
+++ frontend/src/components/Root.js
@@ -5,16 +5,16 @@ import ContentAdd from 'material-ui/svg-icons/content/add';
 import AppToolbar from './AppToolbar';
 import PostList from './PostList';
 const Root = (props) => {
-  const { categories, changeOrder, comments, decScorePost, deletingPost, filter, filterFunc, incScorePost, addPostModal, editPostModal, posts, sort } = props;
+  const { changeOrder1, comments1, decScorePost1, deletingPost1, filter1, filterFunc1, incScorePost1, addPostModal1, editPostModal1, posts1, sort1 } = props;
   return (
     <div>
-	<AppToolbar filterTitle="Category" categories={categories} filterFunc={filterFunc} filter={filter} sortingTitle="Order" sort={sort} changeOrderFunc={changeOrder}
-      />      <PostList comments={comments} decScorePost={decScorePost} deletingPost={deletingPost} filter={filter} incScorePost={incScorePost} editPostModal={editPostModal} posts={posts} sort={sort} />
-      <FloatingActionButton style={{ margin: 20 }} onClick={ addPostModal } >
+	<AppToolbar filterFunc5={filterFunc1} sort5={sort1} changeOrderFunc5={changeOrder1}/>      
+    <PostList comments4={comments1} decScorePost4={decScorePost1} deletingPost4={deletingPost1} filter4={filter1} incScorePost4={incScorePost1} editPostModal4={editPostModal1} posts4={posts1} sort4={sort1} />
+      <FloatingActionButton style={{ margin: 20 }} onClick={ addPostModal1 } >
         <ContentAdd />
       </FloatingActionButton>
     </div>
   );
 };
-Root.propTypes = { categories: PropTypes.array.isRequired, changeOrder: PropTypes.func.isRequired, comments: PropTypes.object.isRequired, decScorePost: PropTypes.func.isRequired, deletingPost: PropTypes.func.isRequired, filter: PropTypes.string.isRequired, filterFunc: PropTypes.func.isRequired, incScorePost: PropTypes.func.isRequired, addPostModal: PropTypes.func.isRequired, editPostModal: PropTypes.func.isRequired, posts: PropTypes.array.isRequired, sort: PropTypes.object.isRequired }
+Root.propTypes = { changeOrder1: PropTypes.func.isRequired, comments1: PropTypes.object.isRequired, decScorePost1: PropTypes.func.isRequired, deletingPost1: PropTypes.func.isRequired, filter1: PropTypes.string.isRequired, filterFunc1: PropTypes.func.isRequired, incScorePost1: PropTypes.func.isRequired, addPostModal1: PropTypes.func.isRequired, editPostModal1: PropTypes.func.isRequired, posts1: PropTypes.array.isRequired, sort1: PropTypes.object.isRequired }
 export default Root;
