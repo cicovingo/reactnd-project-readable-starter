308abd9a1b21cb37799dc5505091c1259fbc0409
cicovingo <sukruokul@gmail.com>
2018-04-04 15:47:38 +0300

    Update Root.js

--- frontend/src/components/Root.js
+++ frontend/src/components/Root.js
@@ -9,7 +9,7 @@ import { fetchPosts, sortOrder } from '../actions/index';
 class Root extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: 2};
+    this.state = {value: 3};
   }
   componentDidMount() {
     if (this.props.match.params.category)
@@ -21,43 +21,47 @@ class Root extends Component {
   	return this.props.sortOrder(sortType);
   }
   handleChange = (event, index, value) => {
-	if(value === 2){
+	if(value === 3){
 		this.sort('voteScore')
-	} else if(value === 3){
+	} else if(value === 2){
 		this.sort('-voteScore')
 	}
 	this.setState({value});
   }
-  fromSort = (key) => {
-	let sortOrder = 1;
-	if (key[0] === '-') {
-		sortOrder = -1;
-		key = key.substr(1);
-	}
-    return this.sortOrderFunc(key, sortOrder);
-  };
-  sortOrderFunc = (key, sortOrder, a, b) => {
-		return sortOrder * ((a[key] < b[key]) ? -1 : (a[key] > b[key]) ? 1 : 0);
-  };
+  
   render() {
     return (
       <div>
-		<DropDownMenu value={this.state.value} onChange={this.handleChange} openImmediately={true}>
-			<MenuItem value={1} primaryText="SortBy" />
-			<MenuItem value={2} primaryText="Popular Post" />
-			<MenuItem value={3} primaryText="Not Popular Post" />
-		</DropDownMenu>
+	<DropDownMenu value={this.state.value} onChange={this.handleChange}>
+		<MenuItem value={1} primaryText="SortBy" />
+		<MenuItem value={2} primaryText="Popular Post" />
+		<MenuItem value={3} primaryText="Not Popular Post" />
+	</DropDownMenu>
         {this.props.posts.map((post) => <AppPost key={post.id} post={post}/>)}
       </div>
     );
   }
 }
+const fromSort = (key) => {
+	let sortOrder = 1;
+	console.log(key);
+	if (key[0] === '-') {
+		sortOrder = -1;
+		key = key.substr(1);
+	}
+    return function (a, b) {
+		return sortOrder * ((a[key] < b[key]) ? -1 : (a[key] > b[key]) ? 1 : 0);
+  }; 
+};
 Root.propTypes = {fetchPosts: PropTypes.func,match: PropTypes.object,posts: PropTypes.array,sortOrder: PropTypes.func};
 const mapStateToProps = ({ post }) => {
   if (post.posts) {
     let posts = Object.keys(post.posts).map((postId) => post.posts[postId]).filter((post) => post);
-    if (posts.sortBy)
-      posts.sort(this.fromSort(posts.sortBy));
+	console.log(posts.sortBy);
+    if (post.sortBy){
+		posts.sort(fromSort(post.sortBy));
+		console.log("bbbbbbbbbbbbbbb");
+	}
     return { posts };
   } else
     return { posts: [] };
