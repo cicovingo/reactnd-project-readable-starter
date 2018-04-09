f96019a3f92be6b1fa75b3a3e67f36853b8d60f8
cicovingo <sukruokul@gmail.com>
2018-04-09 22:00:30 +0300

    Update index.js

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -10,44 +10,79 @@ const token = localStorage.getItem('token') || generateToken();
 axios.defaults.headers.common['Authorization'] = token;
 const axoisPages = {};
 
-const fetchForComments = (comments) => ({type: 'FETCH_COMMENTS',comments});
-export const fetchComments = (parentId) => (dispatch) => {
-  axoisPages.fetcApiComments(parentId).then((comments) => dispatch(fetchForComments(comments)));
-};
-axoisPages.fetcApiComments = (id) => axios.get(`${URL}/posts/${id}/comments`).then((res) => res.data);
-
 const addForComment = (comment) => ({type: 'ADD_COMMENT',comment});
 export const addComment = (comment) => (dispatch) => {
-  axoisPages.addApiComment(comment).then((comment) => dispatch(addForComment(comment)));
+  axoisPages.addPageComment(comment).then((comment) => dispatch(addForComment(comment)));
 };
-axoisPages.addApiComment = (comment) => {
+axoisPages.addPageComment = (comment) => {
   const id = uuid();
   comment = { ...comment, id };
   return axios.post(`${URL}/comments`, comment)
   .then((res) => res.data);
 };
 
+const addForPost = (post) => ({type: 'ADD_POST',post});
+export const addPost = (post) => (dispatch) => {
+  axoisPages.addPagePost(post).then((post) => dispatch(addForPost(post)));
+};
+axoisPages.addPagePost = (post) => {
+  const id = uuid();
+  const timestamp = Date.now();
+  post = { ...post, id, timestamp };
+  return axios.post(`${URL}/posts`, post).then((res) => res.data);
+};
+
 const deleteForComment = (comment) => ({type: 'DELETE_COMMENT',comment});
 export const deleteComment = (commentId) => (dispatch) => {
-  axoisPages.deleteApiComment(commentId).then((comment) => dispatch(deleteForComment(comment)));
+  axoisPages.deletePageComment(commentId).then((comment) => dispatch(deleteForComment(comment)));
 };
-axoisPages.deleteApiComment = (id) => axios.delete(`${URL}/comments/${id}`).then((res) => res.data);
+axoisPages.deletePageComment = (id) => axios.delete(`${URL}/comments/${id}`).then((res) => res.data);
+
+const deleteForPost = (postId) => ({type: 'DELETE_POST',postId});
+export const deletePost = (postId) => (dispatch) => {
+  axoisPages.deletePagePost(postId).then(() => dispatch(deleteForPost(postId)));
+};
+axoisPages.deletePagePost = (id) => axios.delete(`${URL}/posts/${id}`);
+
+const editForComment = (comment) => ({ type: 'EDIT_COMMENT', comment });
+export const editComment = (id, comment) => (dispatch) => {
+  axoisPages.editPageComment(id, comment).then((comment) => dispatch(editForComment(comment)));
+};
+axoisPages.editPageComment = (id, comment) => axios.put(`${URL}/comments/${id}`, comment).then((res) => res.data);
+
+const editForPost = (post) => ({ type: 'EDIT_POST', post });
+export const editPost = (id, post) => (dispatch) => { 
+  axoisPages.editPagePost(id, post).then((post) => dispatch(editForPost(post)));
+};
+axoisPages.editPagePost = (id, post) => axios.put(`${URL}/posts/${id}`, post).then((res) => res.data);
 
 const fetchForCategories = (categories) => ({type: 'FETCH_CATEGORIES',categories});
 export const fetchCategories = () => (dispatch) => {
-  axoisPages.fetchApiCategories().then((categories) => dispatch(fetchForCategories(categories)));
+  axoisPages.fetchPageCategories().then((categories) => dispatch(fetchForCategories(categories)));
 };
-axoisPages.fetchApiCategories = () => axios.get(`${URL}/categories`).then((res) => res.data.categories).then((categories) => categories.map((category) => category.name));
+axoisPages.fetchPageCategories = () => axios.get(`${URL}/categories`).then((res) => res.data.categories).then((categories) => categories.map((category) => category.name));
+
+const fetchForComments = (comments) => ({type: 'FETCH_COMMENTS',comments});
+export const fetchComments = (parentId) => (dispatch) => {
+  axoisPages.fetchPageComments(parentId).then((comments) => dispatch(fetchForComments(comments)));
+};
+axoisPages.fetchPageComments = (id) => axios.get(`${URL}/posts/${id}/comments`).then((res) => res.data);
+
+const fetchForPost = (post) => ({type: 'FETCH_POST',post});
+export const fetchPost = (id) => (dispatch) => {
+  axoisPages.fetchPagePostById(id).then((post) => dispatch(fetchForPost(post)));
+};
+axoisPages.fetchPagePostById = (id) => axios.get(`${URL}/posts/${id}`).then((res) => {return res.data;});
 
 const fetchForPosts = (posts) => ({type: 'FETCH_POSTS',posts});
 export const fetchPosts = (category) => (dispatch) => {
   if(category)
     axoisPages.fetchPostsByCategory(category).then((posts) => dispatch(fetchForPosts(posts)));
   else
-	axoisPages.fetchApiPosts(category).then((posts) => dispatch(fetchForPosts(posts)));
+	axoisPages.fetchPagePosts(category).then((posts) => dispatch(fetchForPosts(posts)));
 };
 axoisPages.fetchPostsByCategory = (category) => axios.get(`${URL}/${category}/posts`).then((res) => fetchCommentCount(res.data));
-axoisPages.fetchApiPosts = () => axios.get(`${URL}/posts`).then((res) => fetchCommentCount(res.data));
+axoisPages.fetchPagePosts = () => axios.get(`${URL}/posts`).then((res) => fetchCommentCount(res.data));
 const fetchCommentCount = (lengthOfArr) => {
   const lengthOfPromise = lengthOfArr.map((post) => axoisPages.fetcApiComments(post.id));
   return axios.all(lengthOfPromise).then((lengthOfResult) => lengthOfResult.map((result) => result.length)).then((lengthArr) => lengthArr.map((length, index) => {
@@ -57,40 +92,16 @@ const fetchCommentCount = (lengthOfArr) => {
   );
 };
 
-const fetchForPost = (post) => ({type: 'FETCH_POST',post});
-export const fetchPost = (id) => (dispatch) => {
-  axoisPages.fetchPostById(id).then((post) => dispatch(fetchForPost(post)));
-};
-axoisPages.fetchPostById = (id) => axios.get(`${URL}/posts/${id}`).then((res) => {return res.data;});
-
-const addForPost = (post) => ({type: 'ADD_POST',post});
-export const addPost = (post) => (dispatch) => {
-  axoisPages.addApiPost(post).then((post) => dispatch(addForPost(post)));
-};
-axoisPages.addApiPost = (post) => {
-  const id = uuid();
-  const timestamp = Date.now();
-  post = { ...post, id, timestamp };
-  return axios.post(`${URL}/posts`, post).then((res) => res.data);
-};
-
-const voteForPost = ({ id, voteScore }) => ({type: 'VOTE_POST',id,voteScore});
-export const votePost = (id, vote) => (dispatch) => {
-  axoisPages.voteApiPost(id, vote).then((post) => dispatch(voteForPost(post)));
-};
-axoisPages.voteApiPost = (id, vote) => axios.post(`${URL}/posts/${id}`, {option: vote}).then((res) => res.data);
-
-const deleteForPost = (postId) => ({type: 'DELETE_POST',postId});
-export const deletePost = (postId) => (dispatch) => {
-	console.log(postId);
-  axoisPages.deleteApiPost(postId).then(() => dispatch(deleteForPost(postId)));
-};
-axoisPages.deleteApiPost = (id) => axios.delete(`${URL}/posts/${id}`);
+export const sortOrder = (sort) => ({type: 'SORT_ORDER',sort});
 
 const voteForComment = ({ id, voteScore }) => ({type: 'VOTE_COMMENT',id,voteScore});
 export const voteComment = (id, vote) => (dispatch) => {
-  axoisPages.voteApiComment(id, vote).then((comment) => dispatch(voteForComment(comment)));
+  axoisPages.votePageComment(id, vote).then((comment) => dispatch(voteForComment(comment)));
 };
-axoisPages.voteApiComment = (id, vote) => axios.post(`${URL}/comments/${id}`,{ option: vote }).then((res) => res.data);
+axoisPages.votePageComment = (id, vote) => axios.post(`${URL}/comments/${id}`,{ option: vote }).then((res) => res.data);
 
-export const sortOrder = (sort) => ({type: 'SORT_ORDER',sort});
+const voteForPost = ({ id, voteScore }) => ({type: 'VOTE_POST',id,voteScore});
+export const votePost = (id, vote) => (dispatch) => {
+  axoisPages.votePagePost(id, vote).then((post) => dispatch(voteForPost(post)));
+};
+axoisPages.votePagePost = (id, vote) => axios.post(`${URL}/posts/${id}`, {option: vote}).then((res) => res.data);
