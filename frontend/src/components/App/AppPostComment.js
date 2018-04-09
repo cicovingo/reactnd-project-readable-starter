22bed4d9434097fbe1a75a46ad6687ee2f854905
cicovingo <sukruokul@gmail.com>
2018-04-09 22:49:41 +0300

    Update AppPostComment.js

--- frontend/src/components/App/AppPostComment.js
+++ frontend/src/components/App/AppPostComment.js
@@ -1,17 +1,27 @@
 import { connect } from 'react-redux';
-import { FaTrashO, FaThumbsOUp, FaThumbsODown } from 'react-icons/lib/fa'; 
+import { FaPencil, FaTrashO, FaThumbsOUp, FaThumbsODown } from 'react-icons/lib/fa'; 
 import FlatButton from 'material-ui/FlatButton'; 
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
+import CreateComment from '../Create/CreateComment';
 import { deleteComment, voteComment } from '../../actions/index';
 
 class AppPostComment extends Component {
+  state = { edit: false }
   deleteForHandle = () => {
     this.props.deleteComment(this.props.comment.id);
   }
+  editForHandle = () => {
+    this.setState({ edit: !this.state.edit });
+    this.props.editComment(this.props.comment.id);
+    <CreateComment edit post={this.props.post}/>
+  }
   handleVote = (vote) => {
     this.props.voteComment(this.props.comment.id, vote);
+  }
+  toggleEdit = () => { 
+    this.setState({ edit: !this.state.edit });
   }
   render() {
     const { comment } = this.props;
@@ -24,6 +34,7 @@ class AppPostComment extends Component {
           </p>
         </div>
         <FlatButton onClick={this.deleteForHandle}><FaTrashO /></FlatButton>
+        <FlatButton onClick={this.editForHandle}><FaPencil /></FlatButton>
 		<FlatButton onClick={() => this.handleVote('upVote')}><FaThumbsOUp /></FlatButton>
         <FlatButton>{comment.voteScore}</FlatButton>
         <FlatButton onClick={() => this.handleVote('downVote')}><FaThumbsODown /></FlatButton>       
